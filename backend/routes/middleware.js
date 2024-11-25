9ba0b105b7568154f673646639f11398ccfc96e6
ajaymeru <123156354+ajaymeru@users.noreply.github.com>
2024-11-25 19:24:00 +0530

    nodemon

--- backend/routes/middleware.js
+++ backend/routes/middleware.js
@@ -2,19 +2,18 @@ const jwt = require('jsonwebtoken');
 const secretKey = 'your_secret_key';
 
 function authenticate(req, res, next) {
-  const token = req.headers['authorization'];
-  if (!token) {
-    return res.status(401).json({ message: 'Access token is required' });
-  }
-
-  try {
-    const decoded = jwt.verify(token, secretKey);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: 'Invalid token' });
-  }
+    const token = req.headers['authorization'];
+    if (!token) {
+        return res.status(401).json({ message: 'Access token is required' });
+    }
+    // decoding
+    try {
+        const decoded = jwt.verify(token, secretKey);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
 }
 
 module.exports = { authenticate };
-    
